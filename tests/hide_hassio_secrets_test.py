88bbfd2f30715f9bc939a335bd1be1c8ce12b3fe
Levi Govaerts <legovaer@me.com>
2023-02-08 05:24:46 +0100

    💚Added additional tests

--- tests/hide_hassio_secrets_test.py
+++ tests/hide_hassio_secrets_test.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-from pre_commit_hooks.hide_hassio_secrets_test import replace_hassio_secrets
+from pre_commit_hooks.hide_hassio_secrets import get_random_string
+from pre_commit_hooks.hide_hassio_secrets import replace_hassio_secrets
 
 
-def test_hide_hassio_secrets():
+def test_get_random_string():
+    assert len(get_random_string()) == 12
+    assert len(get_random_string(10)) == 10
+
+
+def test_replace_hassio_secrets():
     secrets = {'internal_url': 'https://my.homeassistant.io'}
     fake_secrets = replace_hassio_secrets(secrets)
     assert fake_secrets['internal_url'] != 'https://my.homeassistant.io'
